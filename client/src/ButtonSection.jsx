adfb60b51ce8ae61a5c4f9f136146125b7d72987
xennia316 <xennia316@gmail.com>
2022-06-29 22:03:43 +0100

    new changes on button section look

--- client/src/ButtonSection.jsx
+++ client/src/ButtonSection.jsx
@@ -4,7 +4,7 @@ import styles from "./Buttons/button.module.css";
 const button = (param) => {
   return (
     <div className={styles.buttonBody}>
-      <button className={`op ${styles.button}`} onClick={param.onClick}>
+      <button className={`op ${styles.button1}`} onClick={param.onClick}>
         <p className={styles.buttonText} onClick={param.onClick}>
           {param.value}
         </p>
